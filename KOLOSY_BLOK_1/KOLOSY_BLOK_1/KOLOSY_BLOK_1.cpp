e1fc59944986cfd278213ddd9c3c722a6e840d29
Dawid Biś <db310378@student.polsl.pl>
2024-04-24 22:00:35 +0200

    :D

--- KOLOSY_BLOK_1/KOLOSY_BLOK_1/KOLOSY_BLOK_1.cpp
+++ KOLOSY_BLOK_1/KOLOSY_BLOK_1/KOLOSY_BLOK_1.cpp
@@ -3,9 +3,103 @@
 
 #ifdef kolos_0A_sekcja_0
 
+struct Size
+{
+    size_t columns;
+    size_t rows;
+};
+
+int** allocate(uint16_t columns, uint16_t rows)
+{
+
+    int size = 0;
+
+    size |= columns << 16;
+    size |= rows;
+
+    int** tab = new int* [rows];
+    tab[0] = new int{ size };
+
+    tab++;
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        tab[i] = new int[columns];
+    }
+
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            tab[i][j] = i * j;
+        }
+    }
+
+    return tab;
+}
+
+Size getSize(int** tab)
+{
+    int size = tab[-1][0];
+
+    int rows = size & 0x0000ffff;
+    int columns = (size & 0xffff0000) >> 16;
+
+    std::cout << rows << " " << columns;
+
+    return { 0, 0 };
+}
+
+void saveTab(const char* path, int** tab, int16_t columns, int16_t rows)
+{
+    FILE* file = nullptr;
+    file = fopen(path, "wb");
+
+
+    for (size_t i = 0; i < columns; i++)
+    {
+        fwrite(tab[i], sizeof(int), rows, file);
+    }
+
+    fclose(file);
+}
+
+int** readTab(const char* path, int16_t columns, int16_t rows)
+{
+    FILE* file = nullptr;
+    file = fopen(path, "wb");
+
+    auto tab = allocate(columns, rows);
+
+    for (size_t i = 0; i < columns; i++)
+    {
+        fread(tab[i], sizeof(int), rows, file);
+    }
+
+    fclose(file);
+
+    return tab;
+}
+
 int main()
 {
-			
+    constexpr int sexik = 2;
+    const constexpr static int64_t UwU = 3;
+
+    auto tab = allocate(sexik, UwU);
+    saveTab("a.txt", tab, sexik, UwU);
+    auto sex = readTab("a.txt", 2, UwU);
+
+    for (size_t i = 0; i < UwU; i++)
+    {
+        for (size_t j = 0; j < sexik; j++)
+        {
+            std::cout << sex[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
 
 #endif
